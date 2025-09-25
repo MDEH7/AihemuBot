a71c261311920b4077a2f38baf34c73c66631c0c
MDEH7 <mohamedelrayah47@gmail.com>
2025-09-25 20:16:25 +0100

    Added optional member argument to joindate, but its functionality is broken

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import datetime
 import discord
 from discord.ext import commands
-from discord.ext.commands import BadUnionArgument, MissingRequiredArgument
+from discord.ext.commands import BadUnionArgument, MissingRequiredArgument, MemberConverter, UserConverter, UserNotFound
+from typing import Union
 
 intents = discord.Intents.default() # All intents except presences are enabled
 intents.message_content = True
@@ -16,37 +17,48 @@ async def membercount(ctx):
     await ctx.send(f"Member count: {ctx.guild.member_count}") # Displays the total number of users, including other bots
 
 @bot.command()
-async def joindate(ctx, member: str = None):
+async def joindate(ctx, member: Union[discord.User, str] = None):
+    myGuild = bot.get_guild(726849822350508132)
+    discordUserID = 0
+    userID = 0
+    member_name = 0
 
     if member is None: # If the user doesn't input an ID
-        member = ctx.author.id
-
-    try:
-        memberID_int = int(member)
-    except ValueError:
-        await ctx.send(f"Invalid input! "
-                       f"ID can only be numbers.\n"
-                       f"`Usage: $joindate [ID]`")
+        authorID = ctx.author.id
+        authorName = ctx.author.name
+        await ctx.send(f"{authorName} `[{authorID}]` is a member of {myGuild}! :)")
         return
 
-    # If the user supplied ID is more than 19 numbers long or less than 15 numbers
-    if len(str(memberID_int)) > 19 or len(str(memberID_int)) < 15:
-        await ctx.send(f"Invalid ID!\n"
-                       f"`Usage: $joindate [ID]`")
-        return
+    elif isinstance(member, discord.User):
+        discordUserID = member.id
+        member_name = myGuild.get_member(userID or discordUserID or member)
 
 
     else:
-        myGuild = bot.get_guild(726849822350508132)
-        member_name = myGuild.get_member(memberID_int)
+        try:
+            userID = await commands.UserConverter().convert(ctx, member)
+            member_name = myGuild.get_member(userID.id)
+            if len(str(userID)) > 19 or len(str(userID)) < 15:
+                await ctx.send(f"Invalid ID!\n"
+                               f"`Usage: $joindate [ID]`")
+                return
+        except ValueError:
+            await ctx.send(f"Invalid input! "
+                           f"ID can only be numbers.\n"
+                           f"`Usage: $joindate [ID]`")
+            return
+
+        except discord.ext.commands.errors.UserNotFound:
+            await ctx.send(f"Invalid ID!\n"
+                           f"`Usage: $joindate [ID]`")
 
 
     # If a user is not a member of your server
     if member_name is None:
-        await ctx.send(f"<@{member}> `[{member}]` is not a member of {myGuild}!\n"
+        await ctx.send(f"{member.mention} `[{member.id}]` is not a member of {myGuild}!\n"
                        f"Why not invite them here? ¯\\_(ツ)_/¯")
     else:
-        await ctx.send(f"{member_name} `[{member}]` is a member of {myGuild}! :)")
+        await ctx.send(f"{member.name} `[{member.id}]` is a member of {myGuild}! :)")
 
 
 
