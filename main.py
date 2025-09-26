97e3ef9779e121d2b8fe570973a1e3c44a50607d
MDEH7 <mohamedelrayah47@gmail.com>
2025-09-26 22:22:04 +0100

    Added command for changing bot prefix & configuration

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import BadUnionArgument, MissingRequiredArgument, MemberConverter, UserConverter, UserNotFound
 from typing import Union
+import asyncio
 
 intents = discord.Intents.default() # All intents except presences are enabled
 intents.message_content = True
 intents.messages = True
 intents.members = True
 
-bot = commands.Bot(command_prefix='$', intents=intents, case_insensitive=True)
+with open("bot_prefix.txt", "r") as bot_prefix_obj:
+    aihemuBotPrefix = bot_prefix_obj.readline()
+
+bot = commands.Bot(command_prefix=aihemuBotPrefix, intents=intents, case_insensitive=True)
 
 
 @bot.command()
@@ -60,7 +64,63 @@ async def joindate(ctx, member: Union[discord.User, str] = None):
     else:
         await ctx.send(f"{member.name} `[{member.id}]` is a member of {myGuild}! :)")
 
+@bot.command()
+async def changeprefix(ctx, arg: Union[str, None]):
+    if arg is None:
+        await ctx.send(f"Are you trying to break this command? <:neutralexpr:1233524707110948884>\n"
+                       f"This is the correct usage: `{bot.command_prefix}changeprefix [prefix]`")
+        return
 
+    with open("prefix_message.txt", "r") as prefix_msg:
+        p_msg = prefix_msg.readline()
+
+    before = bot.command_prefix
+    with open("bot_prefix.txt", "w") as prefix_obj:
+        prefix_obj.write(arg)
+
+    with open("bot_prefix.txt", "r") as read_prefix:
+        bot.command_prefix = read_prefix.readline()
+
+    await ctx.send(f"You successfully changed the prefix from `{before}` to `{bot.command_prefix}`")
+    if len(arg) > 1 and p_msg == "True":
+        await asyncio.sleep(0.75)
+        await ctx.send(f"-# Yes! The prefix can be longer than 1 character! Why? Because I'm generous!")
+
+
+
+@bot.command()
+async def enableprefixmessage(ctx):
+    with open("prefix_message.txt", "r") as pref_msg:
+        verify_enable = pref_msg.readline()
+        if verify_enable == "True":
+            await ctx.send(f"Prefix message is already enabled!")
+            return
+
+    with open("prefix_message.txt", "w") as prefix_message:
+        prefix_message.write("True")
+    await ctx.send("You enabled the prefix message!")
+
+
+@bot.command()
+async def disableprefixmessage(ctx):
+    with open("prefix_message.txt", "r") as pre_msg:
+        verify_disable = pre_msg.readline()
+        if verify_disable == "False":
+            await ctx.send(f"Prefix message is already disabled!")
+            return
+
+
+    with open("prefix_message.txt", "w") as p_message:
+        p_message.write("False")
+    await ctx.send("You disabled the prefix message!")
+
+
+@bot.command()
+async def botprefix(ctx):
+    with open("bot_prefix.txt", "r") as bot_prefix:
+        prefix = bot_prefix.read(1)
+
+    await ctx.send(f"The bot prefix is `{prefix}`")
 
 @bot.event
 async def on_ready():
